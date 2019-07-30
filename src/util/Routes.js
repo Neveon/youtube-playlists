581cba5b963eef5b97a0953e8c4d3c9f0247bfa4
Neil T <neiltz711@gmail.com>
2019-07-29 23:37:25 -0400

    Fixed routing for all playlists

--- src/util/Routes.js
+++ src/util/Routes.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 // Pages
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
+import Explore from '../pages/Explore';
 import PlaylistItemPage from '../components/PlaylistItemPage';
 
 // Auth
 import AuthRoute from './AuthRoute';
 
-// Redux
-import { connect } from 'react-redux';
-
-const Routes = ({ allPlaylists }) => {
+const Routes = () => {
   return (
     <Router>
       <div className='container'>
@@ -22,28 +19,16 @@ const Routes = ({ allPlaylists }) => {
           <Route exact path='/' component={Home} />
           <AuthRoute exact path='/login' component={Login} />
           <AuthRoute exact path='/signup' component={Signup} />
-          {allPlaylists !== null
-            ? allPlaylists.map(playlist => (
-                <Route
-                  key={playlist.name}
-                  exact
-                  path={`/${playlist.name}`}
-                  component={PlaylistItemPage}
-                />
-              ))
-            : null}
+          <Route exact path='/explore' component={Explore} />
+          <Route
+            exact
+            path='/playlist/:playlist'
+            component={PlaylistItemPage}
+          />
         </Switch>
       </div>
     </Router>
   );
 };
 
-Routes.propTypes = {
-  allPlaylists: PropTypes.array.isRequired
-};
-
-const mapStateToProps = state => ({
-  allPlaylists: state.playlist.allPlaylists
-});
-
-export default connect(mapStateToProps)(Routes);
+export default Routes;
