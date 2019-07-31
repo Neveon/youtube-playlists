10a18eae06bae81bbd0299ee2f8a54e3feade2d0
Neil T <neiltz711@gmail.com>
2019-07-31 13:21:56 -0400

    Added Explore page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,6 +4,9 @@ import PropTypes from 'prop-types';
 import { loginUser } from '../redux/actions/userActions';
 import { Link } from 'react-router-dom';
 
+// Image
+import ExploreImage from '../assets/music-collection.jpg';
+
 const Login = ({ loginUser, user: { loading }, history }) => {
   const [form, setForm] = useState({
     email: '',
@@ -25,67 +28,94 @@ const Login = ({ loginUser, user: { loading }, history }) => {
   };
 
   return (
-    <div className='row center-align'>
-      <h1 className='text-center'>Login</h1>
-      <form className='col s12' onSubmit={onSubmit}>
-        <div className='row'>
-          <div className='input-field col s12'>
-            <input
-              value={form.email}
-              onChange={onChange}
-              type='text'
-              name='email'
-              id='email'
-              className='validate'
-            />
-            <label htmlFor='email'>Email</label>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='input-field col s12'>
-            <input
-              value={form.password}
-              onChange={onChange}
-              type='password'
-              name='password'
-              id='password'
-              className='validate'
-            />
-            <label htmlFor='password'>Password</label>
-          </div>
-        </div>
-
-        <button
-          className='btn waves-effect waves-light'
-          type='submit'
-          disabled={loading}
-        >
-          Login
-          <i className='material-icons right'>send</i>
-        </button>
-        <br />
-        <br />
-        {loading ? (
-          <div className='preloader-wrapper small active'>
-            <div className='spinner-layer spinner-green-only'>
-              <div className='circle-clipper left'>
-                <div className='circle' />
-              </div>
-              <div className='gap-patch'>
-                <div className='circle' />
-              </div>
-              <div className='circle-clipper right'>
-                <div className='circle' />
-              </div>
+    <div className='row'>
+      <Link to='/explore'>
+        <div className='col s12 m6'>
+          <div id='exploreContainer'>
+            <img src={ExploreImage} alt='Explore' className='responsive-img' />
+            <p id='exploreTitle' className='flow-text'>
+              Explore Playlists
+            </p>
+            <i className='material-icons white-text pulse' id='exploreIcon'>
+              explore
+            </i>
+            <br />
+            <div id='exploreContent' className='center-align'>
+              <p>
+                All playlists are unlisted and available publicly to explore.
+                <br />
+                <br />
+                You can create your own playlist here or explore to check out
+                what playlists are out there.
+              </p>
             </div>
           </div>
-        ) : null}
-        <br />
-        <br />
-        <small>
-          Don't have an account? <Link to='/signup'>Sign up here</Link>
-        </small>
-      </form>
+        </div>
+      </Link>
+      <div className='col s12 m6'>
+        <div className='row center-align'>
+          <h1 className='text-center'>Login</h1>
+          <form className='col s12' onSubmit={onSubmit}>
+            <div className='row'>
+              <div className='input-field col s12'>
+                <input
+                  value={form.email}
+                  onChange={onChange}
+                  type='text'
+                  name='email'
+                  id='email'
+                  className='validate'
+                />
+                <label htmlFor='email'>Email</label>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='input-field col s12'>
+                <input
+                  value={form.password}
+                  onChange={onChange}
+                  type='password'
+                  name='password'
+                  id='password'
+                  className='validate'
+                />
+                <label htmlFor='password'>Password</label>
+              </div>
+            </div>
+
+            <button
+              className='btn waves-effect waves-light'
+              type='submit'
+              disabled={loading}
+            >
+              Login
+              <i className='material-icons right'>send</i>
+            </button>
+            <br />
+            <br />
+            {loading ? (
+              <div className='preloader-wrapper small active'>
+                <div className='spinner-layer spinner-green-only'>
+                  <div className='circle-clipper left'>
+                    <div className='circle' />
+                  </div>
+                  <div className='gap-patch'>
+                    <div className='circle' />
+                  </div>
+                  <div className='circle-clipper right'>
+                    <div className='circle' />
+                  </div>
+                </div>
+              </div>
+            ) : null}
+            <br />
+            <br />
+            <small>
+              Don't have an account? <Link to='/signup'>Sign up here</Link>
+            </small>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,6 +4,9 @@ import { connect } from 'react-redux';
 import { signupUser } from '../redux/actions/userActions';
 import PropTypes from 'prop-types';
 
+// Images
+import ExploreImage from '../assets/music-collection.jpg';
+
 const Signup = ({ user: { loading }, signupUser, history }) => {
   const [form, setForm] = useState({
     email: '',
@@ -29,94 +32,119 @@ const Signup = ({ user: { loading }, signupUser, history }) => {
   };
 
   return (
-    <div>
-      <div className='row center-align'>
-        <h1 className='text-center'>Sign up</h1>
-        <form className='col s12' onSubmit={onSubmit}>
-          <div className='row'>
-            <div className='input-field col s12'>
-              <input
-                value={form.email}
-                onChange={onChange}
-                type='text'
-                name='email'
-                id='email'
-                className='validate'
-              />
-              <label htmlFor='email'>Email</label>
+    <div className='row'>
+      <Link to='/explore'>
+        <div className='col s12 m6'>
+          <div id='exploreContainer'>
+            <img src={ExploreImage} alt='Explore' className='responsive-img' />
+            <p id='exploreTitle' className='flow-text'>
+              Explore Playlists
+            </p>
+            <i className='material-icons white-text pulse' id='exploreIcon'>
+              explore
+            </i>
+            <br />
+            <div id='exploreContent' className='center-align'>
+              <p>
+                All playlists are unlisted and available publicly to explore.
+                <br />
+                <br />
+                You can create your own playlist or explore to check out what
+                playlists are out there.
+              </p>
             </div>
           </div>
-          <div className='row'>
-            <div className='input-field col s12'>
-              <input
-                value={form.password}
-                onChange={onChange}
-                type='password'
-                name='password'
-                id='password'
-                className='validate'
-              />
-              <label htmlFor='password'>Password</label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='input-field col s12'>
-              <input
-                value={form.confirmPassword}
-                onChange={onChange}
-                type='password'
-                name='confirmPassword'
-                id='confirmPassword'
-                className='validate'
-              />
-              <label htmlFor='confirmPassword'>Confirm Password</label>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='input-field col s12'>
-              <input
-                value={form.username}
-                onChange={onChange}
-                type='text'
-                name='username'
-                id='username'
-                className='validate'
-              />
-              <label htmlFor='username'>Username</label>
-            </div>
-          </div>
-
-          <button
-            className='btn waves-effect waves-light'
-            type='submit'
-            disabled={loading}
-          >
-            Signup
-            <i className='material-icons right'>send</i>
-          </button>
-          <br />
-          <br />
-          {loading ? (
-            <div className='preloader-wrapper small active'>
-              <div className='spinner-layer spinner-green-only'>
-                <div className='circle-clipper left'>
-                  <div className='circle' />
-                </div>
-                <div className='gap-patch'>
-                  <div className='circle' />
-                </div>
-                <div className='circle-clipper right'>
-                  <div className='circle' />
-                </div>
+        </div>
+      </Link>
+      <div className='col s12 m6'>
+        <div className='row center-align'>
+          <h1 className='text-center'>Sign up</h1>
+          <form className='col s12' onSubmit={onSubmit}>
+            <div className='row'>
+              <div className='input-field col s12'>
+                <input
+                  value={form.email}
+                  onChange={onChange}
+                  type='text'
+                  name='email'
+                  id='email'
+                  className='validate'
+                />
+                <label htmlFor='email'>Email</label>
               </div>
             </div>
-          ) : null}
-          <br />
-          <br />
-          <small>
-            Already have an account? <Link to='login'>Sign in here</Link>
-          </small>
-        </form>
+            <div className='row'>
+              <div className='input-field col s12'>
+                <input
+                  value={form.password}
+                  onChange={onChange}
+                  type='password'
+                  name='password'
+                  id='password'
+                  className='validate'
+                />
+                <label htmlFor='password'>Password</label>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='input-field col s12'>
+                <input
+                  value={form.confirmPassword}
+                  onChange={onChange}
+                  type='password'
+                  name='confirmPassword'
+                  id='confirmPassword'
+                  className='validate'
+                />
+                <label htmlFor='confirmPassword'>Confirm Password</label>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='input-field col s12'>
+                <input
+                  value={form.username}
+                  onChange={onChange}
+                  type='text'
+                  name='username'
+                  id='username'
+                  className='validate'
+                />
+                <label htmlFor='username'>Username</label>
+              </div>
+            </div>
+
+            <button
+              className='btn waves-effect waves-light'
+              type='submit'
+              disabled={loading}
+            >
+              Signup
+              <i className='material-icons right'>send</i>
+            </button>
+            <br />
+            <br />
+            {loading ? (
+              <div className='preloader-wrapper small active'>
+                <div className='spinner-layer spinner-green-only'>
+                  <div className='circle-clipper left'>
+                    <div className='circle' />
+                  </div>
+                  <div className='gap-patch'>
+                    <div className='circle' />
+                  </div>
+                  <div className='circle-clipper right'>
+                    <div className='circle' />
+                  </div>
+                </div>
+              </div>
+            ) : null}
+            <br />
+            <br />
+            <small>
+              Already have an account? <Link to='login'>Sign in here</Link>
+            </small>
+          </form>
+        </div>
       </div>
     </div>
   );
